58ac54da7ce806aa32b2b9686aa66315c8d79cca
thijs <thijs@6d14e641-e035-0410-9c74-c594bd4234a4>
2007-12-22 21:33:05 +0000

    Adding cross-platform build script for py2app/py2exe.

--- setup.py
+++ setup.py
@@ -1,17 +1,53 @@
 # Copyright (c) 2007 The Pair Project. All rights reserved.
+# See LICENSE for details.
+
+"""
+Build script for Pair.
+
+@author: U{Thijs Triemstra<mailto:info@collab.nl>}
+@since: December 2007
+@see: U{http://dev.collab.com/pair}
+"""
 
 from ez_setup import use_setuptools
 
 use_setuptools()
 
+import sys
 from setuptools import setup, find_packages
 
-setup(name = "Pair",
+mainscript = 'pair/bootloader.py'
+
+if sys.platform == 'darwin':
+    extra_options = dict(
+        setup_requires=['py2app>=0.3.6'],
+        app=[mainscript],
+        # Cross-platform applications generally expect sys.argv to
+        # be used for opening files.
+        options=dict(py2app=dict(argv_emulation=True)),
+    )
+elif sys.platform == 'win32':
+    extra_options = dict(
+        setup_requires=['py2exe>=0.6.6'],
+        app=[mainscript],
+    )
+else:
+    extra_options = dict(
+        # Normally unix-like platforms will use "setup.py install"
+        # and install the main script as such
+        scripts=[mainscript],
+    )
+     
+setup(
+    name = "Pair",
     version = "1.0.0",
     description = "AIR for Python",
     url = "http://dev.collab.com/pair",
     packages = find_packages(exclude=["*.tests"]),
     license = "MIT License",
+    extras_require={
+        'wsgi': ['wsgiref']
+    },
     classifiers = [
         "Development Status :: 2 - Pre-Alpha",
         "Natural Language :: English",
@@ -20,4 +56,5 @@ setup(name = "Pair",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
-    ])
+    ],
+    **extra_options)
